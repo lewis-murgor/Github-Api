0179346ec91bff71c220a6818dd5960389f2a4e6
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-13 20:29:15 +0300

    add api url

--- src/app/service/github-request.service.ts
+++ src/app/service/github-request.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRequestService {
 
-  constructor() { }
+  users = new BehaviorSubject<any>([])
+
+  getGithubUsers() {
+    return this.http.get('https://api.github.com/users/?q=${githubUserName}&access_token=${environment.apiKey}').subscribe((response: any)=>{
+      this.users.next(response.data)
+    });
+  }
+
+  getRepositories() {
+    return this.http.get('https://api.github.com/users/repos?access_token=${environment.apiKey}').subscribe((response: any)=>{
+      this.users.next(response.data)
+    });
+  }
+
+  getGithubInformation() {
+    return this.users.asObservable();
+  }
+
+  constructor(private http: HttpClient) { }
+
 }
