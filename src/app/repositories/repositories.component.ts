cb01f83c438235cfdd54ef5d328c342d50d5f374
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-14 02:11:28 +0300

    add input binding

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-repositories',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+
+  @Input() repositories: any
 
   constructor() { }
 
