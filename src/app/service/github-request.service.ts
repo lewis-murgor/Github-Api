69679f19cfa728af53900873110a700de3cbf15a
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-15 20:07:44 +0300

    request for github api

--- src/app/service/github-request.service.ts
+++ src/app/service/github-request.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../classes/user';
+import { Repo } from '../classes/repo';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
+
 
 
 @Injectable({
@@ -9,18 +11,55 @@ import { Observable } from 'rxjs';
 })
 export class GithubRequestService {
 
+  user!: User;
+  repository!: Repo;
+  repoData: any
+  userData: any
+
+
+  constructor(private http: HttpClient) {
+    this.userData = new User("", 0, "", "", new Date)
+    this.repoData = new Repo("", "", "", "", new Date)
+   }
+
+   getGithubInfo(userName: string) {
+
+    this.repoData.length = 0;
+
+    interface ApiResponse {
+      login: string,
+      public_repos: number,
+      avatar_url: string,
+      html_url: string
+    }
+    let promise = new Promise<void>((resolve, reject) => {
+      this.http.get<ApiResponse>("https://api.github.com/users/" + userName + environment.apiKey).toPromise().then(response => {
+        this.user.login = User.login;
+        this.user.public_repos = User.public_repos;
+        this.user.avatar_url = User.avatar_url;
+        this.user.html_url = User.html_url;
   
-
-  getGithubUsers(search:string):Observable<any> {
-    return this.http.get('https://api.github.com/users/${search}?=${environment.apiKey}')
-  }
-
-  getRepositories(search:string):Observable<any> {
-    return this.http.get('https://api.github.com/users/${search}/repositories?=${environment.apiKey}')
-  }
-
-  
-
-  constructor(private http: HttpClient) { }
+        resolve();
+      },
+        error => {
+          this.user.login = "lewis-murgor"
+          this.user.avatar_url = "https://avatars.githubusercontent.com/u/95276296?v=4"
+          this.user.public_repos = 15
+          this.user.html_url = "https://github.com/lewis-murgor"
+          reject(error);
+        }) 
+      this.http.get<any>("https://api.github.com/users/" + userName + "/repos" + environment.apiKey).toPromise().then(response => {
+          for (let i = 0; i < response.length; i++) {
+            this.userData = new Repo(response[i].name, response[i].description, response[i].language, response[i].html_url, response[i].updated);
+            this.repoData.push(this.userData);
+          }
+          resolve();
+        },
+          error => {
+            reject();
+          })
+      })
+      return promise;
+   }
 
 }
