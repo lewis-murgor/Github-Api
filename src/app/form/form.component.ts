5d43a1cacc05563ed5f405187ded64d97805b98d
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-15 20:03:40 +0300

    define functions

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { User } from '../classes/user';
+import { GithubRequestService } from '../service/github-request.service';
+import { UserComponent } from '../user/user.component';
+
 
 @Component({
   selector: 'app-form',
@@ -8,17 +11,26 @@ import { User } from '../classes/user';
 })
 export class FormComponent implements OnInit {
 
-  @Output() inputtedUser = new EventEmitter<User>();
+  @Output() displayUser = new EventEmitter<User>();
 
-  searchUserName() {
-    this.inputtedUser.emit(this.user)
+  user!: User;
+  userName!: string
+  githubService!: GithubRequestService;
+
+  newUser = new User("", 0, "", "", new Date)
+
+  searchUserName(){
+    this.displayUser.emit(this.newUser)
+    this.githubService.getGithubInfo(this.userName)
   }
 
-  user = new User("")
-
-  constructor() { }
+  constructor(githubService: GithubRequestService) {
+    this.githubService = githubService;
+   }
 
   ngOnInit(): void {
+    this.githubService.getGithubInfo(this.userName)
+    this.user = this.githubService.user;
   }
 
 }
