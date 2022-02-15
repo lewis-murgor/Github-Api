d8ff91ab671fa97e77b7f8c241cd18990d3dedbd
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-15 19:55:47 +0300

    modify file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { User } from './classes/user';
-import { GithubRequestService } from './service/github-request.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,25 +8,7 @@ import { GithubRequestService } from './service/github-request.service';
 })
 export class AppComponent {
 
-  userName!: string;
-  githubUser: any;
-  githubRepositories: any;
+  
+  constructor() {}
 
-  constructor(private githubService: GithubRequestService) {}
-
-  findGithub(user: User) {
-    this.userName = user.userName
-    console.log(this.userName)
-
-    this.githubService.getGithubUsers(this.userName).subscribe(data => {
-      this.githubUser = data
-      console.log(data)
-    })
-    
-    this.githubService.getRepositories(this.userName).subscribe(data => {
-      this.githubRepositories = data
-      console.log(data)
-    })
-    
-  }
 }
