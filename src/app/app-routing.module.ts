565edb373c4b9f6be73f815c6bc830e091ceaa53
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-15 19:42:20 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { FormComponent } from './form/form.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'form', component: FormComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'repositories', component: RepositoriesComponent},
+  {path: '**', component: NotFoundComponent},
+
+  
+  { path: '', redirectTo:"user", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
