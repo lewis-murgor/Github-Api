ca4d1a9bae9003e860e14095a2303adb90b973ed
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-15 19:59:35 +0300

    add object properties

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,3 +1,7 @@
 export class User {
-    constructor(public userName: string) {}
+    static login: string;
+  static public_repos: number;
+  static avatar_url: string;
+  static html_url: string;
+    constructor(public login: string, public public_repos: number, public avatar_url: string, public html_url: string, public created_at: Date) {}
 }
