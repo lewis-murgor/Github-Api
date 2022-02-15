d0fa21f4cf45e337f753f95c28dc0ae704041f15
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-15 22:38:03 +0300

    declare pipe and directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { FormComponent } from './form/form.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateDifferencePipe } from './date-difference.pipe';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     RepositoriesComponent,
     NavbarComponent,
     NotFoundComponent,
+    HighlightDirective,
+    DateDifferencePipe,
   ],
   imports: [
     BrowserModule,
