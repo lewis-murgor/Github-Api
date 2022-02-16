0e086b8761965237f5d729104a82e42a1943017a
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-16 11:47:50 +0300

    modify routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { FormComponent } from './form/form.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path: 'form', component: FormComponent},
   {path: 'user', component: UserComponent},
   {path: 'repositories', component: RepositoriesComponent},
-  {path: '**', component: NotFoundComponent},
 
-  
   { path: '', redirectTo:"user", pathMatch:"full"}
 ];
 
