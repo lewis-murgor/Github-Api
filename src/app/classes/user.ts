3cede6828bf7cc2f7aedd4b8cfff797fc374369e
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-22 15:33:03 +0300

    modify user object

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,7 +1,3 @@
 export class User {
-    static login: string;
-  static public_repos: number;
-  static avatar_url: string;
-  static html_url: string;
-    constructor(public login: string, public public_repos: number, public avatar_url: string, public html_url: string, public created_at: Date) {}
+  constructor(public name: string, public login: string, public avatar_url: string, public public_repos: number, public html_url: string, public followers: number, public following: number, public created_at: Date) {}
 }
